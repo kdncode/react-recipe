24ce1e2cd78fdd63fbc28b19db60ea9a8aa05155
kdncode <kdncode@gmail.com>
2018-07-11 23:49:15 -0500

    re-styled colors

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
 		return (
 			<div className="App">
 				<header className="App-header">
-				<h1 className="App-title">Recipe Search</h1>
+				<h1 className="App-title">Explore The Best Recipes</h1>
 				</header>
 				<Form getReceipe={this.getReceipe}/>
 				<Recipes recipes={this.state.recipes}/>
